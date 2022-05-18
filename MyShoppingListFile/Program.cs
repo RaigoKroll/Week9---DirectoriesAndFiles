6ada1916f24b9baf9359c508b2e68149f58cad19
Raigo Kroll <raigo.kroll@gmail.com>
2022-05-18 23:10:40 +0300

    add new projects

---
+++ MyShoppingListFile/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace MyShoppingListFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        //Konsooliäpp, mis lubab või võimaldab kasutajal lisada neid asju MyShoppingList faili
+        {
+            string fileLocation = @"C:\Users\Moniik\samples\ShoppingList";
+            string fileName = @"\\MyShoppingList.txt";
+
+            string[] arrayFromFile = File.ReadAllLines($"{fileLocation}{fileName}");
+            List<string> myShoppingList = arrayFromFile.ToList<string>();
+
+            bool loopActive = true;
+
+            while (loopActive)
+            {
+                Console.WriteLine("Would you like to add a item to your shopping list? Y/N:");
+                char userInput = Convert.ToChar(Console.ReadLine().ToLower());
+
+                if (userInput == 'y')
+                {
+                    Console.WriteLine("Enter your item:");
+                    string userItem = Console.ReadLine();
+                    myShoppingList.Add(userItem);
+                }
+                else
+                {
+                    loopActive = false;
+                    Console.WriteLine("Have a nice day!");
+                }
+
+            }
+            Console.Clear();
+
+            foreach (string item in myShoppingList)
+            {
+                Console.WriteLine($"Your item: {item}");
+            }
+
+            File.WriteAllLines($"{fileLocation}{fileName}", myShoppingList);
+        }
+    }
+}
+
---
+++ ShoppingList/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+namespace ShoppingList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Võimaldab kasutajal luua uue kausta ja uue faili.
+            //Kausta nimi on shopping list ja faili nimi MyShoppingList.txt
+
+            string rootDirectory = @"C:\Users\Moniik\samples";
+            string newDirectory = @"ShoppingList";
+            string fileName = "MyShoppingList.txt";
+
+            Directory.CreateDirectory($"{rootDirectory}\\{newDirectory}");
+            File.Create($"{rootDirectory}\\{newDirectory}\\{fileName}");
+
+            Console.WriteLine(@"Folder created to C:\Users\Moniik\samples with name: ShoppingList");
+            Console.WriteLine("MyShoppingList.txt file is created to folder: ShoppingList");
+        }
+    }
+}
